0564c808b7463aba182c6815e5ef9f6e6fdab3ce
DarkSky <darksky2048@gmail.com>
2019-07-15 12:54:28 +0800

    use as a library

--- waifu2x.cpp
+++ waifu2x.cpp
@@ -4,22 +4,6 @@
 #include <algorithm>
 #include <vector>
 
-#ifdef WIN32
-// image decoder and encoder with wic
-#include "wic_image.h"
-#else // WIN32
-// image decoder and encoder with stb
-#define STB_IMAGE_IMPLEMENTATION
-#define STBI_NO_PSD
-#define STBI_NO_TGA
-#define STBI_NO_GIF
-#define STBI_NO_HDR
-#define STBI_NO_PIC
-#include "stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-#endif // WIN32
-
 using namespace std;
 
 // ncnn
@@ -28,69 +12,83 @@ using namespace std;
 #include "gpu.h"
 
 static const uint32_t waifu2x_preproc_spv_data[] = {
-	#include "waifu2x_preproc.spv.hex.h"
+#include "waifu2x_preproc.spv.hex.h"
 };
 static const uint32_t waifu2x_preproc_fp16s_spv_data[] = {
-	#include "waifu2x_preproc_fp16s.spv.hex.h"
+#include "waifu2x_preproc_fp16s.spv.hex.h"
 };
 #if !defined(NO_INT8_SUPPORT)
 static const uint32_t waifu2x_preproc_int8s_spv_data[] = {
-	#include "waifu2x_preproc_int8s.spv.hex.h"
+#include "waifu2x_preproc_int8s.spv.hex.h"
 };
 #endif
 static const uint32_t waifu2x_postproc_spv_data[] = {
-	#include "waifu2x_postproc.spv.hex.h"
+#include "waifu2x_postproc.spv.hex.h"
 };
 static const uint32_t waifu2x_postproc_fp16s_spv_data[] = {
-	#include "waifu2x_postproc_fp16s.spv.hex.h"
+#include "waifu2x_postproc_fp16s.spv.hex.h"
 };
 #if !defined(NO_INT8_SUPPORT)
 static const uint32_t waifu2x_postproc_int8s_spv_data[] = {
-	#include "waifu2x_postproc_int8s.spv.hex.h"
+#include "waifu2x_postproc_int8s.spv.hex.h"
 };
 #endif
 
+#ifdef WAIFU2X_LITE
+#include "models/models_noise_only.h"
+#else
 #include "models/models.h"
+#endif
 
-class waifu2x_config {
+class waifu2x_config
+{
 public:
 	int noise = 0;
 	int scale = 2;
 	int tilesize = 128;
 	bool cunet = true;
-public:
-	waifu2x_config(int noise = 0, int scale = 2, int tilesize = 400, bool cunet = true) {
-		if (noise >= 0) {
+	waifu2x_config(int noise = 0, int scale = 2, int tilesize = 400, bool cunet = true)
+	{
+		if (noise >= 0)
+		{
 			this->noise = noise;
 		}
-		if (scale >= 1) {
+		if (scale >= 1)
+		{
 			this->scale = scale;
 		}
-		if (tilesize >= 32) {
+		if (tilesize >= 32)
+		{
 			this->tilesize = tilesize;
 		}
 		this->cunet = cunet;
 	}
-	const unsigned char* read_param() {
+	const unsigned char* read_param()
+	{
 		return (this->cunet ? cunet::get_param : upconv_7_anime_style_art_rgb::get_param)(this->noise, this->scale);
 	}
-	const unsigned char* read_model() {
+	const unsigned char* read_model()
+	{
 		return (this->cunet ? cunet::get_model : upconv_7_anime_style_art_rgb::get_model)(this->noise, this->scale);
 	}
-	const int input_blob() {
+	const int input_blob()
+	{
 		return (this->cunet ? cunet::get_input : upconv_7_anime_style_art_rgb::get_input)(this->noise, this->scale);
 	}
-	const int extract_blob() {
+	const int extract_blob()
+	{
 		return (this->cunet ? cunet::get_extract : upconv_7_anime_style_art_rgb::get_extract)(this->noise, this->scale);
 	}
-	const int prepadding() {
+	const int prepadding()
+	{
 		return (this->cunet ? cunet::get_padding : upconv_7_anime_style_art_rgb::get_padding)(this->noise, this->scale);
 	}
 };
 
-class waifu2x_image {
+class waifu2x_image
+{
 public:
-	int prepadding, scale = 2;
+	int prepadding, scale;
 	int w, h, c;
 	int prepadding_bottom, prepadding_right;
 	int xtiles, ytiles;
@@ -100,55 +98,21 @@ public:
 	waifu2x_image(waifu2x_config* config = new waifu2x_config())
 		: prepadding(config->prepadding()), scale(config->scale), TILE_SIZE_X(config->tilesize), TILE_SIZE_Y(config->tilesize),
 		w(0), h(0), c(0), prepadding_bottom(0), prepadding_right(0), xtiles(0), ytiles(0), data(0)
-	{}
-	waifu2x_image(int prepadding, int scale, int tilesize)
-		:prepadding(prepadding), scale(scale), TILE_SIZE_X(tilesize), TILE_SIZE_Y(tilesize),
-		w(0), h(0), c(0), prepadding_bottom(0), prepadding_right(0), xtiles(0), ytiles(0), data(0)
-	{}
-	~waifu2x_image() {
-#ifdef WIN32
-		free(this->data);
-#else
-		stbi_image_free(this->data);
-#endif
+	{
 	}
-#ifdef WIN32
-	void encode(const wchar_t* output) {
-#else
-	void encode(const char* output) {
-#endif
-#ifdef WIN32
-		int ret = wic_encode_image(output, this->buffer.w, this->buffer.h, 3, this->buffer.data);
-#else
-		int ret = stbi_write_png(output, this->buffer.w, outrgb.h, 3, outrgb.data, 0);
-#endif
-		if (ret == 0)
-		{
-			fprintf(stderr, "encode image %ls failed\n", output);
-			return;
-		}
-	}
-#ifdef WIN32
-	void decode(const wchar_t* input) {
-#else
-	void decode(const char* input) {
-#endif
-#ifdef WIN32
-		this->data = wic_decode_image(input, &this->w, &this->h, &this->c);
-#else
-		this->data = stbi_load(input, &this->w, &this->h, &this->c, 3);
-#endif 
-		if (!this->data)
-		{
-			fprintf(stderr, "decode image %ls failed\n", input);
-			return;
-		}
+	void decode(unsigned char* data, int w, int h, int c)
+	{
+		this->data = data;
+		this->w = w;
+		this->h = h;
+		this->c = c;
 		this->calc_config();
 		this->buffer = ncnn::Mat(this->w * this->scale, this->h * this->scale, (size_t)3u, 3);
 	}
 
 private:
-	void calc_config() {
+	void calc_config()
+	{
 		// prepadding
 		int prepadding_bottom = this->prepadding;
 		int prepadding_right = this->prepadding;
@@ -167,9 +131,10 @@ private:
 		this->xtiles = (this->w + this->TILE_SIZE_X - 1) / this->TILE_SIZE_X;
 		this->ytiles = (this->h + this->TILE_SIZE_Y - 1) / this->TILE_SIZE_Y;
 	}
-	};
+};
 
-class waifu2x {
+class waifu2x
+{
 private:
 	ncnn::Net net;
 	ncnn::VulkanDevice* vkdev;
@@ -179,7 +144,8 @@ private:
 	int extract_blob = 0;
 
 public:
-	waifu2x(int gpuid = 0) :vkdev(0), preproc(0), postproc(0) {
+	waifu2x(int gpuid = 0) : vkdev(0), preproc(0), postproc(0)
+	{
 		ncnn::create_gpu_instance();
 
 		int gpu_count = ncnn::get_gpu_count();
@@ -224,16 +190,20 @@ public:
 
 		ncnn::destroy_gpu_instance();
 	}
-	void load_models(const unsigned char* param, const  unsigned char* model) {
+	void load_models(const unsigned char* param, const unsigned char* model)
+	{
 		this->net.load_param(param);
 		this->net.load_model(model);
 	}
-	void set_model_blob(const int input, const  int extract) {
+	void set_model_blob(const int input, const int extract)
+	{
 		this->input_blob = input;
 		this->extract_blob = extract;
 	}
+
 private:
-	void init_proc() {
+	void init_proc()
+	{
 		// initialize preprocess and postprocess pipeline
 		vector<ncnn::vk_specialization_type> specializations(1);
 #ifdef WIN32
@@ -266,8 +236,10 @@ private:
 			else
 				this->postproc->create(waifu2x_postproc_spv_data, sizeof(waifu2x_postproc_spv_data), "waifu2x_postproc", specializations, 2, 8);
 	}
+
 public:
-	void proc_image(waifu2x_image* image) {
+	void proc_image(waifu2x_image* image)
+	{
 		//#pragma omp parallel for num_threads(2)
 		for (int yi = 0; yi < image->ytiles; yi++)
 		{
@@ -281,11 +253,7 @@ public:
 			}
 			else
 			{
-#ifdef WIN32
-				in = ncnn::Mat::from_pixels(image->data + in_tile_y0 * image->w * 3, ncnn::Mat::PIXEL_BGR2RGB, image->w, (in_tile_y1 - in_tile_y0));
-#else
 				in = ncnn::Mat::from_pixels(image->data + in_tile_y0 * image->w * 3, ncnn::Mat::PIXEL_RGB, image->w, (in_tile_y1 - in_tile_y0));
-#endif
 			}
 
 			ncnn::VkCompute cmd(this->vkdev);
@@ -409,173 +377,43 @@ public:
 				ncnn::Mat out;
 				out.create_like(out_gpu, this->net.opt.blob_allocator);
 				out_gpu.download(out);
-#ifdef WIN32
-				out.to_pixels((unsigned char*)image->buffer.data + yi * image->scale * image->TILE_SIZE_Y * image->w * image->scale * 3, ncnn::Mat::PIXEL_RGB2BGR);
-#else
 				out.to_pixels((unsigned char*)image->buffer.data + yi * image->scale * image->TILE_SIZE_Y * image->w * image->scale * 3, ncnn::Mat::PIXEL_RGB);
-#endif
 			}
 		}
 	}
 };
 
-#ifdef WIN32
-#include <wchar.h>
-static wchar_t* optarg = NULL;
-static int optind = 1;
-static wchar_t getopt(int argc, wchar_t* const argv[], const wchar_t* optstring)
+extern "C" waifu2x_config* init_config(int noise, int scale, int tilesize, bool is_cunet)
 {
-	if (optind >= argc || argv[optind][0] != L'-')
-		return -1;
-
-	wchar_t opt = argv[optind][1];
-	const wchar_t* p = wcschr(optstring, opt);
-	if (p == NULL)
-		return L'?';
-
-	optarg = NULL;
-
-	if (p[1] == L':')
-	{
-		optind++;
-		if (optind >= argc)
-			return L'?';
-
-		optarg = argv[optind];
-	}
-
-	optind++;
-
-	return opt;
-}
-#else // WIN32
-#include <unistd.h> // getopt()
-#endif // WIN32
-
-static void print_usage()
-{
-	fprintf(stderr, "Usage: waifu2x-ncnn-vulkan -i infile -o outfile [options]...\n\n");
-	fprintf(stderr, "  -h               show this help\n");
-	fprintf(stderr, "  -i input-image   input image path (jpg/png)\n");
-	fprintf(stderr, "  -o output-image  output image path (png)\n");
-	fprintf(stderr, "  -n noise-level   denoise level (-1/0/1/2/3, default=0)\n");
-	fprintf(stderr, "  -s scale         upscale ratio (1/2, default=2)\n");
-	fprintf(stderr, "  -t tile-size     tile size (>=32, default=400)\n");
-	fprintf(stderr, "  -m model-path    waifu2x model path (default=models-cunet)\n");
-	fprintf(stderr, "  -g gpu-id        gpu device to use (default=0)\n");
+	return new waifu2x_config(noise, scale, tilesize, is_cunet);
 }
 
-#ifdef WIN32
-int wmain(int argc, wchar_t** argv)
-#else
-int main(int argc, char** argv)
-#endif
+extern "C" waifu2x* init_waifu2x(waifu2x_config* config, int gpuid)
 {
-#ifdef WIN32
-	const wchar_t* imagepath = 0;
-	const wchar_t* outputpngpath = 0;
-	int noise = 0;
-	int scale = 2;
-	int tilesize = 400;
-	int gpuid = 0;
-
-	wchar_t opt;
-	while ((opt = getopt(argc, argv, L"i:o:n:s:t:g:h")) != (wchar_t)-1)
-	{
-		switch (opt)
-		{
-		case L'i':
-			imagepath = optarg;
-			break;
-		case L'o':
-			outputpngpath = optarg;
-			break;
-		case L'n':
-			noise = _wtoi(optarg);
-			break;
-		case L's':
-			scale = _wtoi(optarg);
-			break;
-		case L't':
-			tilesize = _wtoi(optarg);
-			break;
-		case L'g':
-			gpuid = _wtoi(optarg);
-			break;
-		case L'h':
-		default:
-			print_usage();
-			return -1;
-		}
-	}
-#else // WIN32
-	const char* imagepath = 0;
-	const char* outputpngpath = 0;
-	int noise = 0;
-	int scale = 2;
-	int tilesize = 400;
-	int gpuid = 0;
-
-	int opt;
-	while ((opt = getopt(argc, argv, "i:o:n:s:t:g:h")) != -1)
-	{
-		switch (opt)
-		{
-		case 'i':
-			imagepath = optarg;
-			break;
-		case 'o':
-			outputpngpath = optarg;
-			break;
-		case 'n':
-			noise = atoi(optarg);
-			break;
-		case 's':
-			scale = atoi(optarg);
-			break;
-		case 't':
-			tilesize = atoi(optarg);
-			break;
-		case 'g':
-			gpuid = atoi(optarg);
-			break;
-		case 'h':
-		default:
-			print_usage();
-			return -1;
-		}
-	}
-#endif // WIN32
-
-	if (!imagepath || !outputpngpath)
-	{
-		print_usage();
-		return -1;
-	}
-
-	if (noise < -1 || noise > 3 || scale < 1 || scale > 2)
-	{
-		fprintf(stderr, "invalid noise or scale argument\n");
-		return -1;
-	}
-
-	if (tilesize < 32)
-	{
-		fprintf(stderr, "invalid tilesize argument\n");
-		return -1;
-	}
-
-#ifdef WIN32
-	CoInitialize(0);
-#endif
-	auto config = waifu2x_config(noise, scale, tilesize, true);
-	auto image = new waifu2x_image(&config);
 	auto processer = new waifu2x(gpuid);
-	processer->load_models(config.read_param(), config.read_model());
-	processer->set_model_blob(config.input_blob(), config.extract_blob());
-	image->decode(imagepath);
+	processer->load_models(config->read_param(), config->read_model());
+	processer->set_model_blob(config->input_blob(), config->extract_blob());
+	return processer;
+}
+
+extern "C" void* proc_image(waifu2x_config* config, waifu2x* processer, unsigned char* data, int w, int h, int c, waifu2x_image*& image)
+{
+	image = new waifu2x_image(config);
+	image->decode(data, w, h, c);
 	processer->proc_image(image);
-	image->encode(outputpngpath);
-	delete processer;
-	return 0;
+	return image->buffer.data;
+}
+
+extern "C" void free_image(waifu2x_image* image)
+{
+	if (image)
+		delete image;
+}
+
+extern "C" void free_waifu2x(waifu2x_config* config, waifu2x* processer)
+{
+	if (config)
+		delete config;
+	if (processer)
+		delete processer;
 }
