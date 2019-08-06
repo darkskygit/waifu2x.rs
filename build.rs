83c0c306f91fe90ea614b96bd2ec8b8de76962aa
DarkSky <darksky2048@gmail.com>
2019-08-06 14:11:11 +0800

    add ncnn-sys

--- build.rs
+++ build.rs
@@ -1,45 +1,20 @@
 use cmake::Config;
-use path_absolutize::Absolutize;
 use std::env;
 use std::fs::create_dir;
 use std::path::PathBuf;
 
 fn main() {
-    let proj_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
-        .absolutize()
-        .unwrap();
-    let vulkan_dir = proj_dir.join("lib").join("vulkan");
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let ncnn_dir = out_dir.join("ncnn");
-    create_dir(&ncnn_dir).unwrap_or_default();
-    let ncnn = Config::new("lib/ncnn")
-        .generator(if cfg!(windows) {
-            "NMake Makefiles"
-        } else {
-            "Unix Makefiles"
-        })
-        .out_dir(ncnn_dir)
-        .static_crt(true)
-        .profile("MinSizeRel")
-        .env("VULKAN_SDK", &vulkan_dir)
-        .define("MSVC_STATIC", "ON")
-        .define("NCNN_VULKAN", "ON")
-        .build();
-    println!(
-        "cargo:rustc-link-search=native={}",
-        ncnn.join("lib").display()
-    );
-    println!("cargo:rustc-link-lib=static={}", "ncnn");
     let waifu2x_dir = out_dir.join("waifu2x");
     create_dir(&waifu2x_dir).unwrap_or_default();
-    let waifu2x = Config::new("lib/waifu2x")
+    let waifu2x = Config::new("waifu2x")
         .out_dir(waifu2x_dir)
         .static_crt(true)
         .profile("MinSizeRel")
-        .env("VULKAN_SDK", &vulkan_dir)
+        .env("VULKAN_SDK", env::var("DEP_NCNN_VULKAN_DIR").unwrap())
         .define("MSVC_STATIC", "ON")
-        .define("INCLUDE_LIST", ncnn.join("include").join("ncnn"))
-        .define("LINK_LIST", ncnn.join("lib"))
+        .define("INCLUDE_LIST", env::var("DEP_NCNN_INCLUDE").unwrap())
+        .define("LINK_LIST", env::var("DEP_NCNN_LIBRARY").unwrap())
         .define(
             if cfg!(feature = "upconv7") {
                 "WAIFU2X_UPCONV7_ONLY"
@@ -58,7 +33,12 @@ fn main() {
     println!("cargo:rustc-link-lib=static={}", "waifu2x-ncnn-vulkan");
     println!(
         "cargo:rustc-link-search=native={}",
-        vulkan_dir.join("lib").display()
+        env::var("DEP_NCNN_LIBRARY").unwrap()
+    );
+    println!("cargo:rustc-link-lib=static={}", "ncnn");
+    println!(
+        "cargo:rustc-link-search=native={}",
+        env::var("DEP_NCNN_VULKAN_LIB").unwrap()
     );
     if cfg!(windows) {
         println!("cargo:rustc-link-lib=static={}", "vulkan-1");
---
+++ ncnn-sys/README.md
@@ -0,0 +1,3 @@
+## ncnn-sys
+
+This crate only build and link [ncnn](https://github.com/Tencent/ncnn) to rust program as a static library, does not contain any bindings.
