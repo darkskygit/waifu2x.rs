fd8c83742e4dc939ae7ca5d91d54d57e8a695573
DarkSky <darksky2048@gmail.com>
2021-08-01 22:44:54 +0800

    outside model

--- models_noise_outside_model.h
+++ models_noise_outside_model.h
@@ -1,49 +1,20 @@
 #pragma once
 
 #include "models-cunet/noise0_model.id.h"
-#include "models-cunet/noise0_model.mem.h"
 #include "models-cunet/noise1_model.id.h"
-#include "models-cunet/noise1_model.mem.h"
 #include "models-cunet/noise2_model.id.h"
-#include "models-cunet/noise2_model.mem.h"
 #include "models-cunet/noise3_model.id.h"
-#include "models-cunet/noise3_model.mem.h"
+
+extern "C" unsigned char *get_waifu2x_param(int noise, int scale, bool cunet);
+extern "C" unsigned char *get_waifu2x_model(int noise, int scale, bool cunet);
 
 namespace cunet {
 	const unsigned char* get_param(int noise, int scale) {
-		switch (scale) {
-		case 1:
-			switch (noise) {
-			case 0:
-				return &noise0_model_param_bin[0];
-			case 1:
-				return &noise1_model_param_bin[0];
-			case 2:
-				return &noise2_model_param_bin[0];
-			case 3:
-				return &noise3_model_param_bin[0];
-			}
-		default:
-			return &noise0_model_param_bin[0];
-		}
+		return get_waifu2x_param(noise, scale, false);
 	}
 
 	const unsigned char* get_model(int noise, int scale) {
-		switch (scale) {
-		case 1:
-			switch (noise) {
-			case 0:
-				return &noise0_model_bin[0];
-			case 1:
-				return &noise1_model_bin[0];
-			case 2:
-				return &noise2_model_bin[0];
-			case 3:
-				return &noise3_model_bin[0];
-			}
-		default:
-			return &noise0_model_bin[0];
-		}
+		return get_waifu2x_model(noise, scale, true);
 	}
 
 	const int get_input(int noise, int scale) {
@@ -89,39 +60,11 @@ namespace cunet {
 
 namespace upconv_7_anime_style_art_rgb {
 	const unsigned char* get_param(int noise, int scale) {
-		switch (scale) {
-		case 1:
-			switch (noise) {
-			case 0:
-				return &noise0_model_param_bin[0];
-			case 1:
-				return &noise1_model_param_bin[0];
-			case 2:
-				return &noise2_model_param_bin[0];
-			case 3:
-				return &noise3_model_param_bin[0];
-			}
-		default:
-			return &noise0_model_param_bin[0];
-		}
+		return get_waifu2x_param(noise, scale, false);
 	}
 
 	const unsigned char* get_model(int noise, int scale) {
-		switch (scale) {
-		case 1:
-			switch (noise) {
-			case 0:
-				return &noise0_model_bin[0];
-			case 1:
-				return &noise1_model_bin[0];
-			case 2:
-				return &noise2_model_bin[0];
-			case 3:
-				return &noise3_model_bin[0];
-			}
-		default:
-			return &noise0_model_bin[0];
-		}
+		return get_waifu2x_model(noise, scale, true);
 	}
 
 	const int get_input(int noise, int scale) {
